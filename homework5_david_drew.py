662e18c05a0ca786c98f47b7ec177797aec919a0
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-03 06:24:38 +0000

    testing

--- homework5_david_drew.py
+++ homework5_david_drew.py
@@ -1,1 +1,29 @@
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+print (colors)
 
+cs1 = colors[0:2]
+print("The first two items in the list are: ", (cs1))
+
+cs2 = colors[2:4]
+print("The middle two items in the list are: ", (cs2))
+
+print("The first and last items in the list are: " + (colors[0]),(colors[5]))
+
+# for bicycleBrand in bicycles:
+#     print("Do you like " + bicycleBrand)
+#     print("I do too!")
+
+# print("Finished")
+
+# numbers = list(range(1,10))
+# print(numbers)
+
+# for number in numbers:
+#     square = number**2
+#     print(square)
+
+
+# ns1 = numbers[2:3]
+# ns2 = numbers[0:9:2]
+# print(ns1)
+# print(ns2)
