4f0a9d099ed573a2b1f3df0a55098356ec2aae85
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-03 19:00:25 +0000

    another commit

--- homework5_david_drew.py
+++ homework5_david_drew.py
@@ -1,13 +1,16 @@
+# list
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 print (colors)
 
+# slices
 cs1 = colors[0:2]
-print("The first two items in the list are: ", (cs1))
+print("The first two items in the list are:", (cs1))
 
 cs2 = colors[2:4]
-print("The middle two items in the list are: ", (cs2))
+print("The middle two items in the list are:", (cs2))
 
-print("The first and last items in the list are: " + (colors[0]), (colors[5]))
+# indexes
+print("The first and last items in the list are:",(colors[0]), (colors[5]))
 
 # tuples
 foods = ("hamburger", "cheeseburger", "pizza", "hotdog", "fries")
